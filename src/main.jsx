090b677fe7542b4c2c7a05d47ac2d56b0fa5ca6b
Pi2608 <ibaxq26@gmail.com>
2024-05-20 13:08:22 +0700

    Merge with Cuong-Fix

--- src/main.jsx
+++ src/main.jsx
@@ -7,9 +7,11 @@ import i18n from "./translation/i18n.js";
 import { I18nextProvider } from "react-i18next";
 import 'antd/dist/reset.css';
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <BrowserRouter>
+    <React.StrictMode>
+        <I18nextProvider i18n={i18n}>
+          <App />
+          </I18nextProvider>
+      </React.StrictMode>
+  </BrowserRouter>
 );
