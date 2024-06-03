63d52e145ad6c9fda863b682598175ed8faa3f99
Popun1 <popun2003@gmail.com>
2024-06-04 00:41:58 +0700

    Trần Anh Sơn - xong phần filter

--- src/pages/Project Management/ProjectMan.jsx
+++ src/pages/Project Management/ProjectMan.jsx
@@ -8,12 +8,16 @@ import {
     FileExcelOutlined,
     EditOutlined,
     DeleteOutlined,
-    CopyOutlined
+    CopyOutlined,
+    DownOutlined
 } from '@ant-design/icons';
+import {
+    DatePicker,
+    Dropdown,
+    
+} from 'antd';
 import './ProjectMan.css';
-import NewProjectModal from './NewProjectModal.jsx';
 import MainLayout from "../../MainLayout/MainLayout.jsx";
-import dayjs from 'dayjs'
 
 const { Header, Sider, Content } = Layout;
 
@@ -41,25 +45,29 @@ function ManagementItem({ icon, title }) {
     );
 }
 
-function ProjectCard({ title, status, position, technology, leader, subLeader, mentor, startDate, releaseDate, issues, onClick }) {
+function ProjectCard({ title, status, position, technology, leader, subLeader, mentor, startDate, releaseDate, issues }) {
+    
     
     // option of status column
     const optionSelect = [
         {
-            value: 'inProcessed',
-            label: 'In processed',
+            value: 'passed',
+            label: 'Passed',
+        },
+        {
+            value: 'failed',
+            label: 'Failed',
+        },
+        {
+            value: 'pending',
+            label: 'Pending',
         },
     ];
 
-    const formatDate = (date) => {
-        return dayjs(date).format('DD MMM YYYY');
-    };
-    
     return (
         <Card 
             className="project-card"
             title={title} 
-            onClick={onClick}
             extra={
             <span className="status">
                 <Select defaultValue={status} options={optionSelect}/>
@@ -72,8 +80,8 @@ function ProjectCard({ title, status, position, technology, leader, subLeader, m
                 <p><b>Mentor: </b>{mentor}</p>
                 <p><b>Group Zalo: <a href="#">Link</a></b></p>
                 <div className="project-deadline">
-                    <p style={{color: "#5DF400"}}><b>Start Date: {formatDate(startDate)}</b></p>
-                    <p style={{color: "#D62222"}}><b>Release Date: {formatDate(releaseDate)}</b></p>
+                    <p style={{color: "#5DF400"}}><b>Start Date: {startDate}</b></p>
+                    <p style={{color: "#D62222"}}><b>Release Date: {releaseDate}</b></p>
                 </div>
                 <div className="project-footer">
                     <div className="members">
@@ -90,10 +98,6 @@ function ProjectCard({ title, status, position, technology, leader, subLeader, m
 }
 
 function ProjectManagement() {
-
-    const [openModal, setOpenModal] = useState(false); // Move modal state inside component
-    const [selectedProject, setSelectedProject] = useState(null); // State to manage the currently selected Project
-
     const projects = [
         {
             title: "Intern System",
@@ -103,8 +107,8 @@ function ProjectManagement() {
             leader: "Leader Name",
             subLeader: "Sub Leader Name",
             mentor: "Mentor Name",
-            startDate: dayjs("05 Jan 2023", 'DD MMM YYYY'),
-            releaseDate: dayjs("05 Apr 2023", 'DD MMM YYYY'),
+            startDate: "05 Jan 2023",
+            releaseDate: "05 Apr 2023",
             issues: 14
         },
         {
@@ -112,11 +116,11 @@ function ProjectManagement() {
             status: "In process",
             position: "Back-End, Front-End, BA, Design",
             technology: ".NET, ReactJS, Trello",
-            leader: "Leader Name",
+            leader: "Ronaldo",
             subLeader: "Sub Leader Name",
             mentor: "Mentor Name",
-            startDate: dayjs("05 Jan 2023", 'DD MMM YYYY').toDate(),
-            releaseDate: dayjs("05 Apr 2023", 'DD MMM YYYY').toDate(),
+            startDate: "05 Jan 2023",
+            releaseDate: "05 Apr 2023",
             issues: 14
         },
         {
@@ -124,20 +128,115 @@ function ProjectManagement() {
             status: "In process",
             position: "Back-End, Front-End, BA, Design",
             technology: ".NET, ReactJS, Trello",
-            leader: "Leader Name",
+            leader: "Messi",
             subLeader: "Sub Leader Name",
             mentor: "Mentor Name",
-            startDate: dayjs("05 Jan 2023", 'DD MMM YYYY').toDate(),
-            releaseDate: dayjs("05 Apr 2023", 'DD MMM YYYY').toDate(),
+            startDate: "05 Jan 2023",
+            releaseDate: "05 Apr 2023",
             issues: 14
         },
     ];
+    const [filteredProjects, setFilteredProjects] = useState(projects)
+    
+    const [selectedProjects, setSelectedProjects] = useState([]);
+    
+    const titleName = [...new Set(filteredProjects.map(title => title.title))];
 
-    const handleOpenModal = (project) => {
-        setOpenModal(true);
-        setSelectedProject(project);
+    const statusChange = [...new Set(filteredProjects.map(projects => projects.status))];
+
+    const positionNames = [...new Set(filteredProjects.map(projects => projects.position))];
+
+    const technologyName = [...new Set(filteredProjects.map(projects => projects.technology))];
+
+    const leaderName = [...new Set(filteredProjects.map(projects => projects.leader))];
+
+    const subLeaderName = [...new Set(filteredProjects.map(projects => projects.subLeader))];
+
+    const mentorName = [...new Set(filteredProjects.map(projects => projects.mentor))];
+
+    const releaseDateChoice = [...new Set(filteredProjects.map(projects => projects.releaseDate))];
+
+    const handleDateChange = (date) => {
+        setSelectedFilters(prevFilters => ({
+            ...prevFilters,
+            releaseDate: date,
+        }));
     };
 
+
+    const [selectedFilters, setSelectedFilters] = useState({
+        title: '',
+        status: '',
+        position: '',
+        technology: '',
+        leader: '',
+        subLeader: '',
+        mentor: '',
+        releaseDate: null,
+    })
+    
+    const createMenu = (type, items) => (
+        <Menu onClick={({ key }) => handleMenuClick(type, key)}>
+            {/* Map over the items to create menu items */}
+            {items.map((item) => (
+                <Menu.Item key={item}>
+                    <div>{item}</div>
+                </Menu.Item>
+            ))}
+        </Menu>
+    );
+
+    const handleMenuClick = (type, key) => {
+        // Update the selectedFilters state with the new filter value
+        setSelectedFilters(prevFilters => ({
+            ...prevFilters,
+            [type]: key,
+        }));
+    };
+
+    const handleSearch = () => {
+        let results = projects;
+        if (selectedFilters.title) {
+            results = results.filter(projects => projects.title === selectedFilters.title)
+        }
+        if (selectedFilters.position) {
+            results = results.filter(projects => projects.position === selectedFilters.position)
+        }
+        if (selectedFilters.status) {
+            results = results.filter(projects => projects.status === selectedFilters.status)
+        }
+        if (selectedFilters.technology) {
+            results = results.filter(projects => projects.technology === selectedFilters.technology)
+        }
+        if (selectedFilters.leader) {
+            results = results.filter(projects => projects.leader === selectedFilters.leader)
+        }
+        if (selectedFilters.subLeader) {
+            results = results.filter(projects => projects.subLeader === selectedFilters.subLeader)
+        }
+        if (selectedFilters.mentor) {
+            results = results.filter(projects => projects.mentor === selectedFilters.mentor)
+        }
+        if (selectedFilters.releaseDate) {
+            results = results.filter(projects => 
+                moment (projects.releaseDate).isSame(selectedFilters.releaseDate, 'day'))
+        }
+        setFilteredProjects(results)
+    }
+
+    const handleClearFilters = () => {
+        setFilteredProjects(projects);
+        setSelectedFilters({
+            title: '',
+            position: '',
+            status: '',
+            technology: '',
+            leader: '',
+            subLeader: '',
+            mentor: '',
+            releaseDate: null,
+        })
+    }
     return (
         <Layout className="project-management-section">
             <header className="header-section">
@@ -157,31 +256,105 @@ function ProjectManagement() {
 
                 <Row gutter={[16, 16]} className="filter-box">
                     <Col span={6}>
-                        <Input placeholder="Enter Name of project" className="filter-input" />
+                        <Select
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                marginTop: 5,
+                                fontSize: 5
+                            }}
+                            placeholder="Enter Name of Project"
+                            value={selectedFilters.title || undefined}
+                            onSelect={(value) => handleMenuClick('title', value)}
+                            options={titleName.map(title => ({ value: title, label: title }))}
+                        />
                     </Col>
                     <Col span={6}>
-                        <Input placeholder="Enter Position" className="filter-input" />
+                    <Select
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                marginTop: 5,
+                                fontSize: 5
+                            }}
+                            placeholder="Enter Postion"
+                            value={selectedFilters.position || undefined}
+                            onSelect={(value) => handleMenuClick('position', value)}
+                            options={positionNames.map(position => ({ value: position, label: position }))}
+                        />
                     </Col>
                     <Col span={6}>
-                        <Input placeholder="Enter Technology" className="filter-input" />
+                    <Select
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                marginTop: 5,
+                                fontSize: 5
+                            }}
+                            placeholder="Enter Technology"
+                            value={selectedFilters.technology || undefined}
+                            onSelect={(value) => handleMenuClick('technology', value)}
+                            options={technologyName.map(technology => ({ value: technology, label: technology }))}
+                        />
                     </Col>
                     <Col span={6}>
-                        <Input placeholder="Enter Leader" className="filter-input" />
+                    <Select
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                marginTop: 5,
+                                fontSize: 5
+                            }}
+                            placeholder="Enter Leader"
+                            value={selectedFilters.leader || undefined}
+                            onSelect={(value) => handleMenuClick('leader', value)}
+                            options={leaderName.map(leader => ({ value: leader, label: leader }))}
+                        />
                     </Col>
                     <Col span={6}>
-                        <Input placeholder="Enter Sub Leader" className="filter-input" />
+                    <Select
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                marginTop: 5,
+                                fontSize: 5
+                            }}
+                            placeholder="Enter Sub Leader"
+                            value={selectedFilters.subLeader || undefined}
+                            onSelect={(value) => handleMenuClick('subLeader', value)}
+                            options={subLeaderName.map(subLeader => ({ value: subLeader, label: subLeader }))}
+                        />
                     </Col>
                     <Col span={6}>
-                        <Input placeholder="Enter Mentor" className="filter-input" />
+                    <Select
+                            style={{
+                                width: '100%',
+                                height: '100%',
+                                marginTop: 5,
+                                fontSize: 5
+                            }}
+                            placeholder="Enter Mentor"
+                            value={selectedFilters.mentor || undefined}
+                            onSelect={(value) => handleMenuClick('mentor', value)}
+                            options={mentorName.map(mentor => ({ value: mentor, label: mentor }))}
+                    />
                     </Col>
-                    <Col span={6}>
-                        <Input placeholder="Enter Release Date" className="filter-input" />
-                    </Col>
+                    <DatePicker
+                    style={{
+                        width: '30%',
+                        height: '100%',
+                        marginTop: 5,
+                        fontSize: 5
+                    }}
+                    placeholder="Enter Release Date"
+                    value={selectedFilters.releaseDate}
+                    onChange={handleDateChange}
+                />
                 </Row>
 
                 <Space className="buttons-section">
-                    <Button icon={<FileExcelOutlined />} className="clean-filters-button">Clean Filters</Button>
-                    <Button type="primary" icon={<SearchOutlined />} className="search-button">Search</Button>
+                    <Button icon={<FileExcelOutlined />} className="clean-filters-button" onClick={handleClearFilters}>Clean Filters</Button>
+                    <Button type="primary" icon={<SearchOutlined />} className="search-button" onClick={handleSearch}>Search</Button>
                 </Space>
 
                 <Space className="action-buttons">
@@ -192,20 +365,15 @@ function ProjectManagement() {
                 </Space>
 
                 <Row gutter={[16, 16]} className="project-list">
-                    {projects.map((project, index) => (
+                    {filteredProjects.map((project, index) => (
                         <Col span={8} key={index}>
-                            <ProjectCard {...project} onClick={() => handleOpenModal(project)} />
+                            <ProjectCard {...project} />
                         </Col>
                     ))}
                 </Row>
 
                 <Pagination className="pagination" total={56} showTotal={total => `1 - 6 of ${total}`} />
             </Content>
-            <NewProjectModal
-                open={openModal}
-                project={selectedProject}
-                onClose={() => setOpenModal(false)} // Ensure you handle closing the modal
-            />
         </Layout>
     );
 }
