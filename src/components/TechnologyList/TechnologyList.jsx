e9002c865014919ccb385d6c501a1b9e4e4de9dc
Tonion <thanhnguyen240202@gmail.com>
2024-06-05 21:18:55 +0700

    Nguyen Ba Thanh - Merge Data-fl-tag-on-nav (Ha Tan Phat) and Show Question (Le Viet Quoc Thinh) to Technology Management

--- src/components/TechnologyList/TechnologyList.jsx
+++ src/components/TechnologyList/TechnologyList.jsx
@@ -1,9 +1,119 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TechnologyList.css';
 import Card from 'react-bootstrap/Card';
 import { Link } from 'react-router-dom';
 import { TiFolderDelete } from "react-icons/ti";
+import { Modal } from "antd";
+import { Tabs } from "antd";
+import Question from "./Question";
+
+const onChange = (key) => {
+    console.log(key);
+};
+
+const items = [
+    {
+        key: "1",
+        label: "Intern",
+        children: (
+            <Question />
+        ),
+    },
+    {
+        key: "2",
+        label: "Fresher",
+        children: (
+            <Question />
+        ),
+    },
+    {
+        key: "3",
+        label: "Junior",
+        children: (
+            <Question />
+        ),
+    },
+    {
+        key: "4",
+        label: "Middle",
+        children: (
+            <Question />
+        ),
+    },
+    {
+        key: "5",
+        label: "Senior",
+        children: (
+
+            <Question />
+
+
+            // <div className="tab-content">
+            //     <div style={{ display: 'flex', flexWrap: 'wrap', justifyContent: 'space-between' }}>
+            //     <div style={{ marginBottom: '20px' }}>
+            //     <label style={{ display: 'block', fontWeight: 'bold', marginBottom: '5px' }}>Question 1</label>
+            //     <Input
+            //           style={{
+            //             width: "420px",
+            //             height: "70px",
+            //             borderRadius: "20px",
+            //           }}
+            //         />
+            //     </div>
+
+            //     <div style={{ marginBottom: '20px' }}>
+            //     <label style={{ display: 'block', fontWeight: 'bold', marginBottom: '5px' }}>Question 2</label>
+            //     <Input
+            //           style={{
+            //             width: "420px",
+            //             height: "70px",
+            //             borderRadius: "20px",
+            //           }}
+            //         />
+            //     </div>
+
+            //     <div style={{ marginBottom: '20px' }}>
+            //     <label style={{ display: 'block', fontWeight: 'bold', marginBottom: '5px' }}>Question 3</label>
+            //     <Input
+            //           style={{
+            //             width: "420px",
+            //             height: "70px",
+            //             borderRadius: "20px",
+            //           }}
+            //         />
+            //     </div>
+
+            //     </div>
+
+            //     <div>
+            //       Add Question
+
+            //     </div>
+            // </div>
+        ),
+    },
+];
+
 const TechnologyList = ({ activeTab }) => {
+    const [open, setOpen] = useState(false);
+
+    const showModal = () => {
+        setOpen(true);
+    };
+
+    const handleOk = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setLoading(false);
+            setOpen(false);
+        }, 3000);
+    };
+
+    const handleCancel = () => {
+        setOpen(false);
+    };
+
+
     const technologies = {
         'Back-End': [
             { title: 'Java', imageUrl: 'https://www.logo.wine/a/logo/Java_(programming_language)/Java_(programming_language)-Logo.wine.svg' },
@@ -30,11 +140,22 @@ const TechnologyList = ({ activeTab }) => {
                         <Card.Img variant="top" src={tech.imageUrl} />
                         <Card.Body>
                             <TiFolderDelete style={{ marginTop: 5, marginRight: 5 }} />
-                            <Link className='link1' style={{ paddingRight: 10, color: "black" }} to="/" variant="primary">Show questions</Link>
+                            <Link className='link1' style={{ paddingRight: 10, color: "black" }} variant="primary" onClick={showModal}>Show questions</Link>
                         </Card.Body>
                     </Card>
                 </div>
             ))}
+            {/* Modal Show Question */}
+            <Modal
+                open={open}
+                onOk={handleOk}
+                onCancel={handleCancel}
+                width={1448}
+                height={447}
+                footer={[]}
+            >
+                <Tabs defaultActiveKey="1" items={items} onChange={onChange} />
+            </Modal>
         </div>
     );
 };
