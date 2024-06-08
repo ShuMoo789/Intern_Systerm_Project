2c28dd942265e1fc14cd56dea0d99c6a4f75b01e
htp2003 <hatanphat08102003@gmail.com>
2024-06-08 14:38:59 +0700

    added responsive style for TechNavigation

--- src/components/TechNavigation/TechNavigation.jsx
+++ src/components/TechNavigation/TechNavigation.jsx
@@ -1,16 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './TechNavigation.css';
-import { Row, Col } from 'antd';
+import { Row, Col, Dropdown, Menu } from 'antd';
+
 const Navigation = ({ activeTab, onTabClick }) => {
+    const [isDropdownOpen, setIsDropdownOpen] = useState(false);
+
     const handleTabClick = (tab) => {
         onTabClick(tab);
+        setIsDropdownOpen(false);
     };
 
     const tabs = ['Back-End', 'Front-End', 'Business Analyst', 'Marketing', 'Design'];
 
+    const dropdownMenu = (
+        <Menu>
+            {tabs.map((tab) => (
+                <Menu.Item key={tab} onClick={() => handleTabClick(tab)}>
+                    {tab}
+                </Menu.Item>
+            ))}
+        </Menu>
+    );
+
     return (
         <div className="navigation-technology-management">
-            <Row justify="center"> {/* Sử dụng Row từ Ant Design và đặt justify="center" để căn giữa */}
+            <Row justify="center">
                 <Col>
                     <div className="navigation-technology-management">
                         <ul className="nav-tabs">
@@ -24,6 +38,21 @@ const Navigation = ({ activeTab, onTabClick }) => {
                                 </li>
                             ))}
                         </ul>
+
+                        <div className="dropdown-container">
+                            <Dropdown
+                                overlay={dropdownMenu}
+                                trigger={['click']}
+                                onVisibleChange={(visible) => setIsDropdownOpen(visible)}
+                                open={isDropdownOpen}
+                                overlayClassName="custom-dropdown-overlay" /* Thêm class cho overlay */
+                                getPopupContainer={(trigger) => trigger.parentNode}
+                            >
+                                <button className="dropdown-toggle custom-dropdown-trigger">
+                                    {activeTab} <span className="caret"></span>
+                                </button>
+                            </Dropdown>
+                        </div>
                     </div>
                 </Col>
             </Row>
