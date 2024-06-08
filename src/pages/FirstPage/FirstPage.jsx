2ee5deec77879cf5be2a3c087ba6932f7fab5547
ShuMoo789 <shuvuive789@gmail.com>
2024-06-08 18:09:26 +0700

    Phap-Fix link first page

--- src/pages/FirstPage/FirstPage.jsx
+++ src/pages/FirstPage/FirstPage.jsx
@@ -1,10 +1,14 @@
-
 import React from "react";
-import "./FirstPage.css";  // Import the CSS file for styling
-import { Avatar, Button, Row, Col } from "antd";  // Import Ant Design components for Avatar and Button
-import userImage from "../../assets/user_image.png";  // Import a user image
-import { DownOutlined, UpOutlined, PlusOutlined, CloseOutlined } from '@ant-design/icons'; // Import the icons from Ant Design
-
+import "./FirstPage.css"; // Import the CSS file for styling
+import { Avatar, Button, Row, Col } from "antd"; // Import Ant Design components for Avatar and Button
+import userImage from "../../assets/user_image.png"; // Import a user image
+import {
+  DownOutlined,
+  UpOutlined,
+  PlusOutlined,
+  CloseOutlined,
+} from "@ant-design/icons"; // Import the icons from Ant Design
+import { Link } from "react-router-dom";
 
 const accounts = [
   {
@@ -25,45 +29,39 @@ const accounts = [
 ];
 
 const First_Page = ({ Accounts }) => {
-
-  const [isCollapsed, setIsCollapsed] = React.useState(true);  // Initialize state for menu collapse
-
+  const [isCollapsed, setIsCollapsed] = React.useState(true); // Initialize state for menu collapse
 
   const toggleMenu = () => {
     setIsCollapsed(!isCollapsed);
   };
-
 
   const handleClose = () => {
     // Add your close functionality here
     console.log("Close button clicked");
   };
 
-
   return (
     <>
       <Row className="container-background">
         <Col span={9}></Col>
         <Col span={4.5}>
-        <Row>
-          <div className="avatar" style={{ paddingLeft: '55px' }}>
-
-            <Avatar
-              className="AvaCurent"
-              shape="square"
-              size={200}
-              src={userImage}
-
-              
-            />
-          </div>
-          {/* <Button type="text" className="close-button" onClick={handleClose}>
+          <Row>
+            <div className="avatar" style={{ paddingLeft: "55px" }}>
+              <Avatar
+                className="AvaCurent"
+                shape="square"
+                size={200}
+                src={userImage}
+              />
+            </div>
+            {/* <Button type="text" className="close-button" onClick={handleClose}>
           <CloseOutlined />
         </Button> */}
-        </Row>
+          </Row>
           <h3 className="UsernameCurent">Natalie Brogan</h3>
-          <Button className="ManageAccount">Manage Your Account</Button>
-
+          <Link to="/Profile">
+            <Button className="ManageAccount">Manage Your Account</Button>
+          </Link>
 
           <div className="container-cover">
             <div className="container-1">
@@ -76,10 +74,17 @@ const First_Page = ({ Accounts }) => {
                 </span>
               </Button>
 
-              <div className={`custom-menu ${isCollapsed ? "" : "show"}`} style={{ width: 400 }}>
+              <div
+                className={`custom-menu ${isCollapsed ? "" : "show"}`}
+                style={{ width: 400 }}
+              >
                 {accounts.map((account, index) => (
                   <div key={index} className="account-item">
-                    <Avatar className="account-avatar" src={account.avatar} style={{ width: 60, height: 60, margin: 10 }} />
+                    <Avatar
+                      className="account-avatar"
+                      src={account.avatar}
+                      style={{ width: 60, height: 60, margin: 10 }}
+                    />
                     <div>
                       <div className="account-name">{account.name}</div>
                       <div className="account-email">{account.email}</div>
@@ -88,11 +93,26 @@ const First_Page = ({ Accounts }) => {
                 ))}
               </div>
               <div className="account-buttons">
-                <Button type="link" className={isCollapsed ? "btn-add-account-hide" : "btn-add-account-show"} icon={<PlusOutlined />}>
-                  Add account
-                </Button>
-                <Button type="link" className={isCollapsed ? "btn-signout-hide" : "btn-signout-show"} icon={<PlusOutlined />}>
-
+                <Link to="/SignIn">
+                  <Button
+                    type="link"
+                    className={
+                      isCollapsed
+                        ? "btn-add-account-hide"
+                        : "btn-add-account-show"
+                    }
+                    icon={<PlusOutlined />}
+                  >
+                    Add account
+                  </Button>
+                </Link>
+                <Button
+                  type="link"
+                  className={
+                    isCollapsed ? "btn-signout-hide" : "btn-signout-show"
+                  }
+                  icon={<PlusOutlined />}
+                >
                   Sign out
                 </Button>
               </div>
