8937cd60a3ffd8a9e696e06dfc37fa026b1ceb4a
ShuMoo789 <shuvuive789@gmail.com>
2024-06-13 00:46:13 +0700

    pháp - fix file navigation.jsx lỗi trùng tên

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -6,40 +6,26 @@ import useViewport from "../../hooks/useViewport";
 import { Input } from "antd";
 
 const Navigation = (props) => {
-  {/*Project-Management*/}
-    const viewPort = useViewport();
-    const isMobile = viewPort.width <= 1024;
+  {
+    /*Project-Management*/
+  }
+  const viewPort = useViewport();
+  const isMobile = viewPort.width <= 1024;
 
-    return (
-        <div className="content-navigation">
-            <div className="header-navigation">
-                <div className="title-name">
-                    {props.titleName}
-                </div>
-                <div className="group-header-navigation">
-                    {!isMobile ? (
-                        <div className="account-setting">
-                            <AccountSetting />
-                        </div>
-                    ) : (
-                        <div className="account-setting-mobile">
-                            <AccountSetting />
-                        </div>
-                    )}
-                </div>
+  return (
+    <div className="content-navigation">
+      <div className="header-navigation">
+        <div className="title-name">{props.titleName}</div>
+        <div className="group-header-navigation">
+          {!isMobile ? (
+            <div className="account-setting">
+              <AccountSetting />
             </div>
-            <div className="navigation">
-                <div className="search-navigation">
-                    <Input placeholder={isMobile ? 'Search' : 'Search for Information'} variant="filled" />
-                </div>
-                <div className="group-button-navigation">
-                    <GroupButton
-                        groupButton={props.groupButton}
-                        onSendEmail={props.onSendEmail}
-                        onScheduleInterview={props.onScheduleInterview}
-                    />
-                </div>
+          ) : (
+            <div className="account-setting-mobile">
+              <AccountSetting />
             </div>
+          )}
         </div>
       </div>
       <div className="navigation">
