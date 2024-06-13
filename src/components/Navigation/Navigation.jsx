84585119681436953b38c9426136d273fe1df0b8
Tonion <thanhnguyen240202@gmail.com>
2024-06-13 12:20:59 +0700

    Nguyen Ba Thanh - fix Navigation

--- src/components/Navigation/Navigation.jsx
+++ src/components/Navigation/Navigation.jsx
@@ -3,7 +3,29 @@ import "./Navigation.css";
 import GroupButton from "../GroupButton/GroupButton";
 import AccountSetting from "../AccountSetting/AccountSetting";
 import useViewport from "../../hooks/useViewport";
-import { Input } from "antd";
+import { Input, Avatar } from "antd";
+import {
+    UserOutlined,
+    BellOutlined,
+    SettingOutlined
+} from '@ant-design/icons';
+import userImage from "../../assets/user_image.png";
+
+function UserInfo({ name, role, avatarSrc }) {
+    return (
+        <div className="user-info">
+            <div className="avatar-section">
+                <Avatar size={54} src={avatarSrc} icon={<UserOutlined />} />
+                <div className="user-details">
+                    <p className="username">{name}</p>
+                    <p className="role">{role}</p>
+                </div>
+            </div>
+            <SettingOutlined className="setting-icon" />
+        </div>
+    );
+}
+
 
 const Navigation = (props) => {
     const viewPort = useViewport()
@@ -11,7 +33,7 @@ const Navigation = (props) => {
 
     return (
         <div className="content-navigation">
-            <div className="header-navigation">
+            {/* <div className="header-navigation">
                 <div className="title-name">
                     {props.titleName}
                 </div>
@@ -22,7 +44,12 @@ const Navigation = (props) => {
                         <AccountSetting />
                     </div>)}           
                 </div>
-            </div>            
+            </div>  */}
+            <header className="header-section">
+                <h1 className="header-title">{props.titleName}</h1>
+                {isMobile ? <SettingOutlined className="setting-icon" /> : <UserInfo name="Natalie Brogan" role="Admin" avatarSrc={userImage} />}
+            </header>
+
             <div className="navigation">
                 <div className="search-navigation">
                     {/* {isMobile ? 'Search' : 'Search for Information'} */}
