32300eb757efb367f78d0553187ac2da29dab4e1
ShuMoo789 <shuvuive789@gmail.com>
2024-06-17 21:52:16 +0700

    link router cho khung chat

--- src/pages/GroupZaloManagementDetails/GroupZaloManagementDetails.jsx
+++ src/pages/GroupZaloManagementDetails/GroupZaloManagementDetails.jsx
@@ -16,20 +16,20 @@ import {
   ExclamationOutlined,
   EllipsisOutlined,
   LikeOutlined,
-  
 } from "@ant-design/icons";
-import { Col, Row, Input, Flex, Typography, Popover,Button } from "antd";
+import { Col, Row, Input, Flex, Typography, Popover, Button } from "antd";
 const { Title } = Typography;
-import data from '@emoji-mart/data'
-import Picker from '@emoji-mart/react'
-import './GroupZaloManagementDetails.css'; 
+import data from "@emoji-mart/data";
+import Picker from "@emoji-mart/react";
+import "./GroupZaloManagementDetails.css";
 
 import SenderContainer from "../../components/ChatContainer/SenderContainer";
 import ReceiverContainer from "../../components/ChatContainer/ReceiverContainer";
 import { useTranslation } from "react-i18next";
+import { Link } from "react-router-dom";
 
 const GroupZaloManagementDetails = () => {
-  const {t} = useTranslation()
+  const { t } = useTranslation();
   const groupButton = [
     {
       color: "#6537B1",
@@ -97,141 +97,155 @@ const GroupZaloManagementDetails = () => {
   };
   return (
     <>
-        <Navigation
-          titleName={t("Group Zalo Management")}
-          groupButton={groupButton}
-          onSendEmail={handleOpenCreateGroup}
-        />
-        <Row >
-          <Col className="viewZaloback" style={ {maxHeight: "100px"}} span={1}>
-            <Row span ={12} className="viewZalo-hide viewZalo-hover" onClick={handleBackToGroupZalo}>
-              <p className="viewZalo no-select">{t("View in Zalo")}</p>
-            </Row>
-
-            <Row span ={12} className= "backTo-hide backTo-hover" onClick={handleBackTo}>
+      <Navigation
+        titleName={t("Group Zalo Management")}
+        groupButton={groupButton}
+        onSendEmail={handleOpenCreateGroup}
+      />
+      <Row>
+        <Col className="viewZaloback" style={{ maxHeight: "100px" }} span={1}>
+          <Row
+            span={12}
+            className="viewZalo-hide viewZalo-hover"
+            onClick={handleBackToGroupZalo}
+          >
+            <p className="viewZalo no-select">{t("View in Zalo")}</p>
+          </Row>
+          <Link to="/GroupZaloManagement">
+            <Row
+              span={12}
+              className="backTo-hide backTo-hover"
+              onClick={handleBackTo}
+            >
               <p className=" viewZalo no-select">{t("Back to")}</p>
             </Row>
-
-          </Col>
-          <Col
-            style={{
-              display: "flex",
-              flexDirection: "column",
-              height: "65vh",
-              width: "92%",
-              backgroundColor: "white",
-              borderTopLeftRadius: "20px",
-              borderTopRightRadius: "20px",
-              padding: "20px",
-              overflowY: "auto",
-            }}
-          >
-            <div style={{ flex: "1 1 auto" }}>
-              <Flex
-                justify="space-between"
+          </Link>
+        </Col>
+        <Col
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            height: "65vh",
+            width: "92%",
+            backgroundColor: "white",
+            borderTopLeftRadius: "20px",
+            borderTopRightRadius: "20px",
+            padding: "20px",
+            overflowY: "auto",
+          }}
+        >
+          <div style={{ flex: "1 1 auto" }}>
+            <Flex
+              justify="space-between"
+              style={{
+                flexWrap: "wrap",
+                alignItems: "center",
+              }}
+            >
+              <div />
+              <div
                 style={{
-                  flexWrap: "wrap",
-                  alignItems: "center",
+                  flex: "1 1 auto",
+                  textAlign: "center",
                 }}
               >
-                <div />
+                <Title level={2} style={{ margin: 0 }}>
+                  Designer_FU_SP24
+                </Title>
+                <Title level={5} style={{ margin: 0 }}>
+                  50 {t("members")}
+                </Title>
+              </div>
+              <div style={{ flex: "0 0 auto" }}>
+                <SearchOutlined />
+                <a href="" style={{ marginRight: "20px" }}>
+                  {t("View in Zalo")}
+                </a>
+              </div>
+            </Flex>
+
+            <SenderContainer
+              message={t("Are you done with the work? Reply soon")}
+              avatar="https://picsum.photos/200/300"
+            />
+            {messages.map((message, index) => (
+              <div key={index}>
+                <ReceiverContainer message={message.text} />
+              </div>
+            ))}
+          </div>
+        </Col>
+      </Row>
+      <Row>
+        <Col span={1}></Col>
+        <Col
+          style={{
+            width: "92%",
+          }}
+        >
+          <div className="chat-input-container chat-input-container-top">
+            <div style={{ marginLeft: "5px" }}></div>
+            <PictureFilled className="icon" />
+            <CameraOutlined className="icon" />
+            <LinkOutlined className="icon" />
+            <ClockCircleOutlined className="icon" />
+            <ExclamationOutlined className="icon" />
+            <EllipsisOutlined className="icon" />
+          </div>
+
+          <form onSubmit={handleSubmit}>
+            <Input
+              value={inputValue}
+              onChange={handleInputChange}
+              placeholder="Type a message"
+              className="chat-input chat-input-bottom"
+              bordered={false}
+              suffix={
                 <div
-                  style={{
-                    flex: "1 1 auto",
-                    textAlign: "center",
-                  }}
+                  style={{ display: "flex", gap: "10px", alignItems: "center" }}
                 >
-                  <Title level={2} style={{ margin: 0 }}>
-                    Designer_FU_SP24
-                  </Title>
-                  <Title level={5} style={{ margin: 0 }}>
-                    50 {t("members")}
-                  </Title>
-                </div>
-                <div style={{ flex: "0 0 auto" }}>
-                  <SearchOutlined />
-                  <a href="" style={{ marginRight: "20px" }}>
-                    {t("View in Zalo")}
-                  </a>
-                </div>
-              </Flex>
-
-              <SenderContainer
-                message={t("Are you done with the work? Reply soon")}
-                avatar="https://picsum.photos/200/300"
-              />
-              {messages.map((message, index) => (
-                <div key={index}>
-                  <ReceiverContainer message={message.text} />
-                </div>
-              ))}
-            </div>
-          </Col>
-        </Row>
-        <Row>
-          <Col span={1}></Col>
-          <Col
-            style={{
-              width: "92%",
-
-            }}
-          >
-            <div className="chat-input-container chat-input-container-top">
-              <div style={{ marginLeft: "5px" }}></div>
-              <PictureFilled className="icon"/>
-              <CameraOutlined className="icon"/>
-              <LinkOutlined className="icon" />
-              <ClockCircleOutlined className="icon" />
-              <ExclamationOutlined className="icon" />
-              <EllipsisOutlined className="icon" />
-              
-            </div>
-
-            <form onSubmit={handleSubmit}>
-              <Input
-                value={inputValue}
-                onChange={handleInputChange}
-                placeholder="Type a message"
-                className="chat-input chat-input-bottom"
-
-                bordered={false}
-                suffix={
-                  <div style={{ display: "flex", gap: "10px", alignItems: 'center' }}>
-                    <Popover
-                      content={<Picker data={data} onEmojiSelect={addEmoji} theme="light" />}
-                      trigger="click"
-                      visible={emojiPickerVisible}
-                      onVisibleChange={() => setEmojiPickerVisible(!emojiPickerVisible)}
-                    >
-                      <SmileOutlined
-                        style={{ cursor: "pointer", fontSize: "25px" }}
-                        onClick={() => setEmojiPickerVisible(!emojiPickerVisible)}
+                  <Popover
+                    content={
+                      <Picker
+                        data={data}
+                        onEmojiSelect={addEmoji}
+                        theme="light"
                       />
-                    </Popover>
-                    {inputValue.trim() ? (
-                      <SendOutlined
-                        onClick={() => handleSendMessage(inputValue)}
-                        style={{
-                          cursor: "pointer",
-                          fontSize: "25px",
-                          marginRight: "5px",
-                        }}
-                      />
-                    ) : (
-                      <LikeOutlined
-                        onClick={handleLike}
-                        style={{ cursor: "pointer", fontSize: "25px" }}
-                      />
-                    )}
-                  </div>
-                }
-              />
-            </form>
-          </Col>
-        </Row>
+                    }
+                    trigger="click"
+                    visible={emojiPickerVisible}
+                    onVisibleChange={() =>
+                      setEmojiPickerVisible(!emojiPickerVisible)
+                    }
+                  >
+                    <SmileOutlined
+                      style={{ cursor: "pointer", fontSize: "25px" }}
+                      onClick={() => setEmojiPickerVisible(!emojiPickerVisible)}
+                    />
+                  </Popover>
+                  {inputValue.trim() ? (
+                    <SendOutlined
+                      onClick={() => handleSendMessage(inputValue)}
+                      style={{
+                        cursor: "pointer",
+                        fontSize: "25px",
+                        marginRight: "5px",
+                      }}
+                    />
+                  ) : (
+                    <LikeOutlined
+                      onClick={handleLike}
+                      style={{ cursor: "pointer", fontSize: "25px" }}
+                    />
+                  )}
+                </div>
+              }
+            />
+          </form>
+        </Col>
+      </Row>
     </>
   );
 };
 
 export default GroupZaloManagementDetails;
-
